66b120b150448d9ea18be76bb900d8f469c2f469
LeBronJaimes1 <aarav.gupta01@icloud.com>
2026-01-12 16:41:47 -0800

    added motor and limit

--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -4,13 +4,26 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 
 public class Storage extends SubsystemBase {
   /** Creates a new Storage. */
+
+  
   TalonFX motor1 = new TalonFX(0);
+  DigitalInput limitSwitch = new DigitalInput(1);
+   public boolean getLimitSwitch(){
+    return limitSwitch.get();
+  }
+
+  public void move(double Speed) {
+    motor1.setControl(new DutyCycleOut(Speed));
+  }
   public Storage() {}
 
   @Override
