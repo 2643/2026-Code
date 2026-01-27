24902220034d541dd593bab51e1e92a9729e1a81
Ishva <ishvakishore@gmail.com>
2026-01-26 16:17:14 -0800

    did auto aim

--- src/main/java/frc/robot/subsystems/turret.java
+++ src/main/java/frc/robot/subsystems/turret.java
@@ -5,11 +5,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+
 
 public class Turret extends SubsystemBase {
   /** Creates a new turretx. */
@@ -20,6 +24,16 @@ public class Turret extends SubsystemBase {
   DigitalInput limitY = new DigitalInput(1);
   public double target = 0;
   public double pos;
+
+  public boolean isVisible;
+  public double yaw;
+  public double area;
+  public double tx;
+  public double ty;
+  public double fiducialID;
+  public double range;
+  private final String limelightName = "limelight";
+  private final String limelightURL = "http://10.26.43.200:5801/";
   MotionMagicVoltage motion = new MotionMagicVoltage(0);
   public void position() {
       var motionmagicconfigs = configs.MotionMagic;
@@ -36,6 +50,38 @@ public class Turret extends SubsystemBase {
       motorY.getConfigurator().apply(configs);
       motorX.setPosition(0);
       motorY.setPosition(0);
+  }
+  public String getLimelightURL() {
+    return limelightURL;
+  }
+
+  public double getTX() {
+    return tx;
+  }
+
+  public double getTY() {
+    return ty;
+  }
+  public void updateData() {
+    isVisible = LimelightHelpers.getTV(limelightName);
+    yaw = LimelightHelpers.getTX(limelightName);
+    tx = LimelightHelpers.getTX(limelightName);  // Horizontal offset (same as yaw)
+    ty = LimelightHelpers.getTY(limelightName);  // Vertical offset
+    area = LimelightHelpers.getTA(limelightName);
+    fiducialID = LimelightHelpers.getFiducialID(limelightName);
+        
+    SmartDashboard.putBoolean("Has Target", isVisible);
+    SmartDashboard.putNumber("Target Yaw", yaw);
+    SmartDashboard.putNumber("Limelight TX", tx);
+    SmartDashboard.putNumber("Limelight TY", ty);
+    SmartDashboard.putNumber("Target Area", area);
+    SmartDashboard.putNumber("Fiducial ID", fiducialID);
+    SmartDashboard.putString("Limelight Stream", limelightURL);
+  }
+  public void autoAlign(){
+    if (isVisible == true) {
+      motorX.setControl(new DutyCycleOut(-tx/30));
+    }
   }
   public void moveToPosY(double target){
     target = pos;
@@ -72,5 +118,7 @@ public class Turret extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    updateData();
+    autoAlign();
   }
 }
