e496db2df8549e31af0043a5b0c0ef4992af70b8
LeBronJaimes1 <aarav.gupta01@icloud.com>
2026-01-28 16:53:15 -0800

    Finished

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;1
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -4,7 +4,7 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,26 +13,46 @@ public class Storage extends SubsystemBase {
   /** Creates a new Storage. */
 
   
-  TalonFX motor1 = new TalonFX(0);
-  DigitalInput limitSwitch = new DigitalInput(1);
+  public TalonFX storage = new TalonFX(0);
+  public DigitalInput limitSwitch = new DigitalInput(1);
   // public boolean getLimitSwitch(){
   //   return limitSwitch.get();
   // }
 
-  public void SetMotorPosition(double Position) {
-    motor1.setControl(new MotionMagicVoltage(Position));
+  public void SetMotorSpeed(double Speed) {
+    storage.setControl(new DutyCycleOut(Speed));
   }
   public Storage() {}
 
   public double getPosition() {
-    return motor1.getPosition().getValueAsDouble();
+    return storage.getPosition().getValueAsDouble();
+  }
+
+  States state = States.ATTACK;
+  public enum States {
+    ATTACK,
+    DEFENSE,
+
   }
 
   @Override
   public void periodic() {
-    if (limitSwitch.get()) {
-      SetMotorPosition(getPosition()+0.1);
+    if(limitSwitch.get()) {
+      SetMotorSpeed(getPosition()+0.1);
     }
-    // This method will be called once per scheduler run
+    
+
+  switch (state) {
+    case ATTACK:
+    break;
+    case DEFENSE:
+    break;
   }
+  }
+  
+  public boolean getLimitValue(){
+    return limitSwitch.get();
+  }
+  
 }
+
